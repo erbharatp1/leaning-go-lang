a17a95b825c267cd7ea289e62ffcffecb23bbccd
bharatpatel <bharat.patel@thoughtworks.com>
2026-03-03 23:58:45 +0530

    feat: Add Note module with CLI for creating, displaying, and saving notes

---
+++ Note-data-go/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"Note-data-go/note"
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func getNoteData() (string, string, error) {
+	title, err := getUserInfo("Notes title:")
+	if err != nil {
+
+		return "", "", err
+	}
+	content, err := getUserInfo("Notes content:")
+	if err != nil {
+		return "", "", err
+	}
+	return title, content, nil
+}
+
+func main() {
+	title, content, err := getNoteData()
+
+	note, err := note.New(title, content)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	note.DisplayNote()
+	err = note.SaveNote()
+	if err != nil {
+		fmt.Println("Error during saving the note:", err)
+		return
+	}
+	fmt.Println("Title:", title)
+	fmt.Println("Content:", content)
+	//	fmt.Println("Title:", title)
+	//fmt.Println("Content:", content)
+}
+
+func getUserInfo(promptTest string) (string, error) {
+	fmt.Print("\n", promptTest)
+	//var input string
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	//text = strings.TrimSpace(reader, "\r")
+	if err != nil {
+		return "", err
+	}
+	//reader = strings.TrimSpace(reader, "\n")
+	//reader = strings.TrimSpace(reader, "\r")
+	return text, nil
+}
---
+++ Note-data-go/note/note.go
@@ -0,0 +1,43 @@
+package note
+
+import (
+	json2 "encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Note struct {
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	createdAt time.Time
+}
+
+func (note Note) DisplayNote() {
+	fmt.Printf("Your Titled Note is %v and content is %v \n", note.Title, note.Content)
+}
+func (note Note) SaveNote() error {
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName)
+	note.createdAt = time.Now()
+	json, err := json2.Marshal(note)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	return os.WriteFile(fileName+".json", json, 0644)
+
+}
+
+func New(title, content string) (Note, error) {
+	if title == "" || content == "" {
+		return Note{}, errors.New("Pls enter title and content")
+	}
+	return Note{
+		Title:     title,
+		Content:   content,
+		createdAt: time.Now(),
+	}, nil
+}
